bc7c59b88584a03407024feba1fe03ca1875e92b
Kamen Nikolov <kamen.nikolov@edifabric.com>
2023-09-25 14:20:15 +0100

    Updated to EdiFabric 10.6.7

--- NET Framework 4.8/EdiFabric.Examples.NCPDP.Script.Demo/Program.cs
+++ NET Framework 4.8/EdiFabric.Examples.NCPDP.Script.Demo/Program.cs
@@ -2,6 +2,7 @@
 using EdiFabric.Core.Model.Edi.ErrorContexts;
 using EdiFabric.Examples.NCPDP.Script.Common;
 using EdiFabric.Framework.Readers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,7 +21,15 @@ namespace EdiFabric.Examples.NCPDP.Script.Demo
 
             //  If you need a different NCPDP version or transaction, please contact us at https://support.edifabric.com/hc/en-us/requests/new, EdiFabric supports all versions and transactions for NCPDP.
 
-            SerialKey.Set(Config.TrialSerialKey);
+            try
+            {
+                SerialKey.Set(Config.TrialSerialKey, true);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("Can't set token"))
+                    throw new Exception("Your trial has expired! To continue using EdiFabric SDK you must purchase a plan from https://www.edifabric.com/pricing.html");
+            }
             //  Uncomment and then comment out the line above if you wish to use distributed cache for tokens
             //  TokenFileCache.Set();
 
